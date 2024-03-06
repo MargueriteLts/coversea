44e257da41f793167979f6c76fbba804bf07b5ee
MargueriteLts <marguerite.lorthiois@outlook.fr>
2024-03-06 12:53:03 +0300

    un peu de style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './index.css'
-// import jpg from './images/image.jpg'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const image = new Image()
-  // image.src = jpg
-  // document.querySelector('.images').appendChild(image)
 })
